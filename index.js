7b7d6254f71738fb52d280747bafaee13763aef6
Kevin <kevin.velazquez.pz@gmail.com>
2025-07-21 12:45:41 -0400

    Updated UI, added TODO

---
+++ index.js
@@ -0,0 +1,32 @@
+// TODO: HECK WHAT DO I EVEN DO?!?!
+
+// Get the card API in here
+// API should get a new deck
+// API should draw a card
+
+// TODO: Actual game rules and logic
+
+// New game: Get a new deck, draw two cards for player and dealer
+// Draw one card face up, have one hidden
+// Should update ASCII UI
+
+// TODO: Hit button
+// Update when player hits
+// Draw a new card, update UI
+// Check for bust [over >21]
+
+// TODO: Stand button
+// Update when player stands
+// Dealer draws cards until reaching 17 or higher
+// Check for winner
+
+// TODO: Score
+// number of cards = value of card
+// face cards = 10
+// Ace = 1 or 11 [player's choice]
+// write a calculateScore(hand) function
+// deal with blackjack [21 with two cards]
+
+// how card ASCII using API responses
+// Dispaly player and dealer hands scores
+//
